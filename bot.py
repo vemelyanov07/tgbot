f02b74201a9da75366ff11ec8bd77dec6ba06c18
vemelyanov07 <vladislav-emelya@mail.ru>
2022-10-13 11:49:10 +0300

    fix bags

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-from email import message
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.utils.markdown import text, code, italic
-from aiogram.types import ParseMode, InputMediaPhoto, CallbackQuery
+from aiogram.types import ParseMode
 from aiogram.types.message import ContentType
 from sqlalchemy import func
 from config import TOKEN
@@ -15,107 +14,83 @@ from kb import Standart_keyboard, Delivery, inline_keyboard1,\
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
 #начало работы с ботом(выбор кальянов)
 @dp.message_handler(commands=['start'])
 async def proccess_start_command(message: types.Message):
-    await message.reply("Доброго времени суток! Выберите вид кальяна.", reply_markup=Standart_keyboard)
-
-
-# @dp.message_handler()
-# async def hookah(message: types.Message):
-#     if message.text == "Стандарт" or "Премиум" or "Премиум +":
-#         await message.answer("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-#         if message.text == "Свежий" or "Кислый" or "Сладкий" or "Пряный" or "Десертный" or "Авторский":
-#             await message.answer("Выберите время доставки.", reply_markup=time_of_deliveo)
-#             if message.text == "Привезти в течение 60 минут." or "Привезти в течение 120 минут." or "Привезти в течение 180 минут.":
-#                 await message.answer("Данные для доставки.", reply_markup=Delivery)
-
-
-# @dp.message_handler()
-# async def hookah_taste(message: types.Message):
-#     if message.text == "Свежий" or "Кислый" or "Сладкий" or "Пряный" or "Десертный" or "Авторский":
-#         await message.answer("Выберите время доставки.", reply_markup=time_of_deliveo)
-#     else:
-#         await message.answer("Выберите время доставки.", reply_markup=time_of_deliveo)
-
-# @dp.message_handler()
-# async def hookah_time_of_deliveo(message: types.Message):
-#     if message.text == "Привезти в течение 60 минут." or "Привезти в течение 120 минут." or "Привезти в течение 180 минут.":
-#         await message.answer("Данные для доставки.", reply_markup=Delivery)
-
-    #     if message.text == "Премиум":
-    #         await message.answer("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-    #     else:
-    #         if message.text == "Премиум +":
-    #             await message.answer("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-@dp.message_handler()
-async def hookah_taste(message: types.Message):
-    if message.text == "Стандарт":
-        await message.answer("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-    if message.text == "Премиум":
-        await message.answer("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-    if message.text == "Премиум +":
-        await message.answer("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-    if message.text == "Свежий":
-        await message.answer("Выберите время для доставки.", reply_markup=time_of_deliveo)
-    if message.text == "Кислый":
-        await message.answer("Выберите время для доставки.", reply_markup=time_of_deliveo)
-    if message.text == "Сладкий":
-        await message.answer("Выберите время для доставки.", reply_markup=time_of_deliveo)
-    if message.text == "Пряный":
-        await message.answer("Выберите время для доставки.", reply_markup=time_of_deliveo)
-    if message.text == "Десертнй":
-        await message.answer("Выберите время для доставки.", reply_markup=time_of_deliveo)
-    if message.text == "Авторский":
-        await message.answer("Выберите время для доставки.", reply_markup=time_of_deliveo)
-    if message.text == "Привезти в течение 60 минут.":
-        await message.answer("Данные для доставки.", reply_markup=Delivery)
-    if message.text == "Привезти в течение 120 минут.":
-        await message.answer("Данные для доставки.", reply_markup=Delivery)
-    if message.text == "Привезти в течение 180 минут.":
-        await message.answer("Данные для доставки.", reply_markup=Delivery)
-    if message.text == "Ваш номер для связи?":
-        await message.answer("Спасибо. Ваш заказ принят.")
-
-
+    await message.reply("Доброго времени суток! Выберите вид кальяна ⚖️", reply_markup=Standart_keyboard)
+    await bot.send_photo(message.from_user.id, photo=open('static\hookah.jpg', 'rb'))
 
 #выбор вкуса кальяна
 @dp.message_handler(commands=['taste'])
 async def proccess_taste_command(message: types.Message):
-    await message.reply("Выберите вкус кальяна.", reply_markup=taste_keyboard)
-
-
-#доступный выбор действия
-@dp.message_handler(commands=['help'])
-async def proccess_help_command(message: types.Message):
-    await message.reply("Выберите вид кальяна.",
-                        reply_markup=inline_keyboard1) 
-    await message.reply("Выберите вкус кальяна.",
-                        reply_markup=inline_keyboard2) 
-    await message.reply("Данные для доставки.",
-                        reply_markup=inline_keyboard3)
-    await message.reply("Выберите время для доставки.",
-                        reply_markup=inline_keyboard4)
-
-#доставка
-@dp.message_handler(commands=['delivery'])
-async def proccess_delivery_command(message: types.Message):
-    await message.reply("Данные для доставки.", reply_markup=Delivery) 
+    await message.reply("Выберите вкус кальяна 💎", reply_markup=taste_keyboard)
 
 #время доставки
 @dp.message_handler(commands=['time'])
 async def proccess_time_command(message: types.Message):
-    await message.reply("Выберите время для доставки.", reply_markup=time_of_deliveo)
+    await message.reply("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+
+#доставка
+@dp.message_handler(commands=['delivery'])
+async def proccess_delivery_command(message: types.Message):
+    await message.reply("Данные для доставки 📜", reply_markup=Delivery)
+
+# спасибо за заказ
+@dp.message_handler(commands=['thank'])
+async def proccess_thank_command(message: types.Message):
+    await message.answer("Спасибо. Ваш заказ принят. Менеджер свяжется с Вами в ближайшее время.")
+
+#доступный выбор действия
+@dp.message_handler(commands=['help'])
+async def proccess_help_command(message: types.Message):
+    await message.reply("Выберите вид кальяна 🔥",
+                        reply_markup=inline_keyboard1) 
+    await message.reply("Выберите вкус кальяна 🔥",
+                        reply_markup=inline_keyboard2) 
+    await message.reply("Данные для доставки 🔥",
+                        reply_markup=inline_keyboard3)
+    await message.reply("Выберите время для доставки 🔥",
+                        reply_markup=inline_keyboard4)
+
+
+@dp.message_handler()
+async def hookah_taste(message: types.Message):
+    if message.text == "Стандарт ✈️":
+        await bot.send_photo(message.from_user.id, photo=open('static\standart.jpg', 'rb'))
+        await message.answer("Выберите вкус кальяна 💎", reply_markup=taste_keyboard)
+    if message.text == "Премиум 🚀":
+        await bot.send_photo(message.from_user.id, photo=open('static\premium.jpg', 'rb'))
+        await message.answer("Выберите вкус кальяна 💎", reply_markup=taste_keyboard)
+    if message.text == "Премиум + 🛸":
+        await bot.send_photo(message.from_user.id, photo=open('static\premium_plus.jpg', 'rb'))
+        await message.answer("Выберите вкус кальяна 💎", reply_markup=taste_keyboard)
+    if message.text == "Свежий 🧊🌱":
+        await message.answer("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+    if message.text == "Кислый 🍋🍇":
+        await message.answer("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+    if message.text == "Сладкий 🥝🥭":
+        await message.answer("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+    if message.text == "Пряный 🥬🥦":
+        await message.answer("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+    if message.text == "Десертный 🍫🍦":
+        await message.answer("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+    if message.text == "Авторский 🍔🌭":
+        await message.answer("Выберите время для доставки 🎁", reply_markup=time_of_deliveo)
+    if message.text == "Привезти в течение 60 минут 6️⃣0️⃣":
+        await message.answer("Данные для доставки 📜", reply_markup=Delivery)
+    if message.text == "Привезти в течение 120 минут 1️⃣2️⃣0️⃣":
+        await message.answer("Данные для доставки 📜", reply_markup=Delivery)
+    if message.text == "Привезти в течение 180 минут 1️⃣8️⃣0️⃣":
+        await message.answer("Данные для доставки 📜", reply_markup=Delivery)
 
 # заказ принят
 # @dp.message_handler()
 # async def proccess_order_command(message: types.Message):
-#     message_text1 = text(
-#         ("Спасибо. Ваш заказ принят."),
-#         )   
-#     await message.answer(message_text1, parse_mode=ParseMode.MARKDOWN)
-    
+#     if message.text == None:
+#         message_text = text(
+#             ("Спасибо. Ваш заказ принят."),
+#             )   
+#     await message.answer(message_text, parse_mode=ParseMode.MARKDOWN)
 
 #повтор действий
 @dp.message_handler()
@@ -125,8 +100,7 @@ async def echo_messege(message: types.Message):
         italic("\nНапоминаю, какие есть виды действий:"),
         code("/help")
         )   
-    await message.reply(message_text, parse_mode=ParseMode.MARKDOWN)   
-
+    await message.reply(message_text, parse_mode=ParseMode.MARKDOWN)  
 
 
 if __name__ == "__main__":
--- kb.py
+++ kb.py
@@ -1,77 +1,74 @@
 from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.emoji import emojize
 
-
 #кнопки выбора кальяна
-button1 = KeyboardButton("Стандарт")
-button2 = KeyboardButton("Премиум")
-button3 = KeyboardButton("Премиум +")
+button1 = KeyboardButton("Стандарт ✈️")
+button2 = KeyboardButton("Премиум 🚀")
+button3 = KeyboardButton("Премиум + 🛸")
 Standart_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, 
                                         one_time_keyboard=True).add(
                                         button1, button2, button3,
                                         )
 
 #Кнопки вкуса кальянов
-button4 = KeyboardButton("Свежий")
-button5 = KeyboardButton("Кислый")
-button6 = KeyboardButton("Сладкий")
-button7 = KeyboardButton("Пряный")
-button8 = KeyboardButton("Десертный")
-button9 = KeyboardButton("Авторский")
+button4 = KeyboardButton("Свежий 🧊🌱")
+button5 = KeyboardButton("Кислый 🍋🍇")
+button6 = KeyboardButton("Сладкий 🥝🥭")
+button7 = KeyboardButton("Пряный 🥬🥦")
+button8 = KeyboardButton("Десертный 🍫🍦")
+button9 = KeyboardButton("Авторский 🍔🌭")
 taste_keyboard = ReplyKeyboardMarkup(resize_keyboard=True,
                                         one_time_keyboard=True).add(button4, button5, 
                                         button6, button7, button8, button9)
 
 #кнопка доставки
-button10 = KeyboardButton("Данные для доставки.")
+button10 = KeyboardButton("Данные для доставки 📜")
 Delivery = ReplyKeyboardMarkup(resize_keyboard=True).add(
-    KeyboardButton("Ваш номер для связи?", request_contact=True)
+    KeyboardButton("Ваш номер для связи? 📱", request_contact=True),
 ).add(
-    KeyboardButton("Где Вы находитесь?", request_location=True)
+    KeyboardButton("Где Вы находитесь? 🧭", request_location=True)
 )
 
-
 #кнопки времени доставки
-button11 = KeyboardButton("Выберите время доставки.")
+button11 = KeyboardButton("Выберите время доставки ⏰")
 time_of_deliveo = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(
-    KeyboardButton("Привезти в течение 60 минут.")
+    KeyboardButton("Привезти в течение 60 минут 6️⃣0️⃣")
 ).add(
-    KeyboardButton("Привезти в течение 120 минут.")
+    KeyboardButton("Привезти в течение 120 минут 1️⃣2️⃣0️⃣")
 ).add(
-    KeyboardButton("Привезти в течение 180 минут.")
+    KeyboardButton("Привезти в течение 180 минут 1️⃣8️⃣0️⃣")
 )
 
 #кнопки внутри бота
-inline_button1 = InlineKeyboardButton("Стандарт", callback_data='button1')
-inline_button2 = InlineKeyboardButton("Премиум", callback_data='button2')
-inline_button3 = InlineKeyboardButton("Премиум +", callback_data='button3')
+inline_button1 = InlineKeyboardButton("Стандарт ✈️", callback_data='button1')
+inline_button2 = InlineKeyboardButton("Премиум 🚀", callback_data='button2')
+inline_button3 = InlineKeyboardButton("Премиум + 🛸", callback_data='button3')
 
 inline_keyboard1 = InlineKeyboardMarkup().add(inline_button1, 
                                         inline_button2, inline_button3)
 
-inline_button4 = InlineKeyboardButton("Свежий", callback_data='button4')
-inline_button5 = InlineKeyboardButton("Кислый", callback_data='button5')
-inline_button6 = InlineKeyboardButton("Сладкий", callback_data='button6')
-inline_button7 = InlineKeyboardButton("Пряный", callback_data='button7')
-inline_button8 = InlineKeyboardButton("Десертный", callback_data='button8')
-inline_button9 = InlineKeyboardButton("Авторский", callback_data='button9')
+
+inline_button4 = InlineKeyboardButton("Свежий 🧊🌱", callback_data='button4')
+inline_button5 = InlineKeyboardButton("Кислый 🍋🍇", callback_data='button5')
+inline_button6 = InlineKeyboardButton("Сладкий 🥝🥭", callback_data='button6')
+inline_button7 = InlineKeyboardButton("Пряный 🥬🥦", callback_data='button7')
+inline_button8 = InlineKeyboardButton("Десертный 🍫🍦", callback_data='button8')
+inline_button9 = InlineKeyboardButton("Авторский 🍔🌭", callback_data='button9')
 
 inline_keyboard2 = InlineKeyboardMarkup().add(inline_button4, 
                                         inline_button5, inline_button6, inline_button7, 
                                         inline_button8, inline_button9)
 
 
-inline_button10 = InlineKeyboardButton("Ваш номер для связи?", request_location=True, callback_data='button11')
-inline_button11 = InlineKeyboardButton("Где Вы находитесь?", request_location=True, callback_data='button12')
+inline_button10 = InlineKeyboardButton("Ваш номер для связи? 📱", request_location=True, callback_data='button11')
+inline_button11 = InlineKeyboardButton("Где Вы находитесь? 🧭", request_location=True, callback_data='button12')
 
 inline_keyboard3 = InlineKeyboardMarkup().add(inline_button10,
                                             inline_button11)
 
 
-inline_button12 = InlineKeyboardButton("Привезти в течение 60 минут.", callback_data='button12')
-inline_button13 = InlineKeyboardButton("Привезти в течение 120 минут.", callback_data='button13')
-inline_button14 = InlineKeyboardButton("Привезти в течение 180 минут.", callback_data='button14')
+inline_button12 = InlineKeyboardButton("Привезти в течение 60 минут 6️⃣0️⃣", callback_data='button12')
+inline_button13 = InlineKeyboardButton("Привезти в течение 120 минут 1️⃣2️⃣0️⃣", callback_data='button13')
+inline_button14 = InlineKeyboardButton("Привезти в течение 180 минут 1️⃣8️⃣0️⃣", callback_data='button14')
 
 inline_keyboard4 = InlineKeyboardMarkup().add(inline_button12).add(inline_button13).add(inline_button14)
-
-
